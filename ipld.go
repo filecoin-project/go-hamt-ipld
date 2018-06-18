2b0f1af9cbd5fb6eccc8762862b3610c0060e73d
Fritz Schneider <fritz@protocol.ai>
2018-06-18 08:49:25 -1000

    lint

--- ipld.go
+++ ipld.go
@@ -21,7 +21,7 @@ import (
 // THIS IS ALL TEMPORARY CODE
 
 func init() {
-        cbor.RegisterCborType(cbor.BigIntAtlasEntry)
+	cbor.RegisterCborType(cbor.BigIntAtlasEntry)
 	cbor.RegisterCborType(Node{})
 	cbor.RegisterCborType(Pointer{})
 
