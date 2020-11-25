0536fe1d3f55206845aa14262f38722c44b80ed0
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-11-25 16:16:35 -0500

    Update module version in preparation for breaking changes

--- gen/main.go
+++ gen/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	hamt "github.com/filecoin-project/go-hamt-ipld/v2"
+	hamt "github.com/filecoin-project/go-hamt-ipld/v3"
 )
 
 func main() {
