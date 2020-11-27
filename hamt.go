c3602342fbefeecaabf7096cb07c7caeecb90bb3
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-11-27 14:48:18 -0500

    Rework comments to describe new caching behavior

--- hamt.go
+++ hamt.go
@@ -109,33 +109,29 @@ type Pointer struct {
 	KVs  []*KV   `refmt:"v,omitempty"`
 	Link cid.Cid `refmt:"l,omitempty"`
 
-	// cache is a pointer to an in-memory Node, which may or may not be present,
-	// and corresponds to the Link field, which also may or may not be present.
+	// cache is a pointer to an in-memory Node, which may or may not be
+	// present, and corresponds to the Link field, which also may or may not
+	// be present.
 	//
-	// If present, the cached Node should be semantically substitutable with the Link field.
-	// It makes no sense for a cache Node to be present if KVs is set.
-	// Link might not be set, if cache is present and is describing data that has never yet been serialized and stored.
+	// If present, the cached Node should be semantically substitutable with
+	// the Link field. It makes no sense for a cache Node to be present if KVs
+	// is set. Link might not be set, if cache is present and is describing
+	// data that has never yet been serialized and stored.
 	//
-	// `loadChild` will short circut to returning this node if the pointer isn't nil;
-	// `loadChild` will also set this pointer when loading a node it wasn't yet present.
-	// `Flush` on a `Node` will iterate through each `Pointer` and `Put` its cache node if it's set
-	// (and also recurse to `Flush` on that `Node`), and then nil it -- in other words,
-	// `Flush` both writes out the cached data and zeros the cache in the same stroke.
-	// `Flush` will also assign `Link` in the process of `Put`'ing the 'cache' data.
-	// `Copy` will copy any cached nodes (and any Link fields).
+	// `loadChild` will short circut to return this node if the pointer isn't
+	// nil;
+	// `loadChild` will also set this pointer when loading a node that wasn't
+	// yet present cached.
+	// `Flush` on a `Node` will iterate through each `Pointer` and `Put` its
+	// cache node if:
+	// 1. The Pointer's cache is not nil
+	// 2. The Pointer's dirty flag is true
+	// (and also recurse to `Flush` on that `Node`) -- in other words,
+	// `Flush` writes out the cached data
+	// `Flush` will assign `Link` in the process of `Put`'ing the 'cache' data.
+	// `Copy` will copy any cached nodes, Link fields and dirty flags.
 	//
-	// `Link` becomes defined either when you `Flush` or
-	// when a new shard is created during the course of `modifyValue`.
-	//
-	// TODO(rvagg): we should check that this is actually used optimally. Flush()
-	// performs a save of all of the cached nodes, but both Copy() and loadChild()
-	// will set them. In the case of loadChild() we're not expecting a mutation so
-	// a save is likely going to mean we incur unnecessary serialization when
-	// we've simply inspected the tree. Copy() will only set a cached form if
-	// it already exists on the source. It's unclear exactly what Flush() is good
-	// for in its current form. Users may also need an advisory about memory
-	// usage of large graphs since they don't have control over this outside of
-	// Flush().
+	// `Link` becomes defined on`Flush`
 	cache *Node
 	// dirty flag to indicate that the cached node needs to be flushed
 	dirty bool
@@ -464,21 +460,23 @@ func (n *Node) checkSize(ctx context.Context) (uint64, error) {
 	return totsize, nil
 }
 
-// Flush has two effects: it (partially!) persists data, and simultaneously drops it from memory.
+// Flush has two effectis, it (partially!) persists data and resets dirty flag
 //
 // Flush operates recursively, telling each "cache" child node to flush;
 // Put'ing that "cache" node to the store;
 // updating this node's Link to the CID resulting from the store Put;
-// dropping the "cache" pointer to that node;
+// clearing the dirty flag of that pointer to flase
 // and then returning.
 // Flush doesn't operate unless there's a "cache" node.
 //
-// ("cache" nodes were previously storing either updated values,
-// or, simply storing previously loaded data; these are not disambiguated.)
+// "cache" nodes were previously storing either updated values,
+// or, simply storing previously loaded data; these are disambiguated by the
+// dirty flag which is true when the cache node's data has not been persisted
 //
 // Notice that Flush _does not_ Put _this node_.
-// To fully persist changes, the caller still needs to Put this node to the store themselves,
-// and store the new resulting Link wherever they expect the updated HAMT to be seen.
+// To fully persist changes, the caller still needs to Put this node to the
+// store themselves, and store the new resulting Link wherever they expect the
+// updated HAMT to be seen.
 func (n *Node) Flush(ctx context.Context) error {
 	for _, p := range n.Pointers {
 		if p.cache != nil && p.dirty {
@@ -501,9 +499,7 @@ func (n *Node) Flush(ctx context.Context) error {
 // Set key k to value v, where v is has a MarshalCBOR(bytes.Buffer) method to
 // encode it.
 //
-// This may or may not cause immediate writes to the store,
-// depending on whether a new subshard is created.
-// Regardless, to fully commit the change, it is necessary to Flush the root Node,
+// To fully commit the change, it is necessary to Flush the root Node,
 // and then additionally Put the root node to the store itself,
 // and save the resulting CID wherever you expect the HAMT root to persist.
 func (n *Node) Set(ctx context.Context, k string, v interface{}) error {
@@ -640,9 +636,11 @@ func (n *Node) modifyValue(ctx context.Context, hv *hashBits, k []byte, v *cbg.D
 	if child.isShard() {
 		// if isShard, we have a pointer to a child that we need to load and
 		// delegate our modify operation to.
-		// Note that this loadChild operation will cause the loaded node to be "cached";
-		// it is an eventual Flush passing back over this "cache" node which causes the updates
-		// made to the in-memory "cache" node to eventually be persisted.
+		// Note that this loadChild operation will cause the loaded node to be
+		// "cached" and this pointer to be marked as dirty;
+		// it is an eventual Flush passing back over this "cache" node which
+		// causes the updates made to the in-memory "cache" node to eventually
+		// be persisted.
 		chnd, err := child.loadChild(ctx, n.store, n.bitWidth, n.hash)
 		if err != nil {
 			return err
